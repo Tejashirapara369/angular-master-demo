0e54872efdd0d0c5ba79f96d22e3f6d58061a3da
mac <mac@Tejas-mac.local>
2022-06-17 21:13:49 +0530

    array form implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    loadChildren: () => import('./array-form/array-form.module').then(m => m.ArrayFormModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
